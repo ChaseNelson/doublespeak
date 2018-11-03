9a210207ad446c974a7e1edc062a245b6c3768fb
Chase Nelson <Chase.a.nelson@gmail.com>
2018-11-03 17:47:26 -0500

    Update Login.js

--- Login.js
+++ Login.js
@@ -18,19 +18,12 @@ import {
 
 // Init Firebase
 const firebaseConfig = {
-<<<<<<< 67419df7123eb30db764c0c9e34ad62346547165
+
   apiKey: "",
   authDomain: "",
   databaseURL: "",
   projectId: "",
   storageBucket: "",
-=======
-  apiKey: "AIzaSyDtJxu-PAdSB5ahIY66eI0ziESkllxcJnE",
-  authDomain: "doublespeak-ba592.firebaseapp.com",
-  databaseURL: "https://doublespeak-ba592.firebaseio.com",
-  projectId: "doublespeak-ba592",
-  storageBucket: "doublespeak-ba592.appspot.com",
->>>>>>> created working login page
 }
 
 firebase.initializeApp(firebaseConfig);
@@ -48,7 +41,6 @@ export default class App extends Component {
   }
 
   signUpUser = (email, password) => {
-<<<<<<< 67419df7123eb30db764c0c9e34ad62346547165
     if (password.length < 6) {
       alert('Password must be at least 6 characters!')
       return
@@ -76,37 +68,6 @@ export default class App extends Component {
     .catch(err => {
       alert(err.message)
     })
-=======
-    try {
-      if (password.length < 6) {
-        alert('Password must be at least 6 characters!')
-        return
-      }
-      firebase.auth().createUserWithEmailAndPassword(email.trim(), password)
-      this.loginUser(email, password)
-    } catch(err) {
-      console.error(err)
-    }
-  }
-
-  loginUser = (email, password) => {
-    try {
-      if (email.length === 0 || password.length === 0) {
-        alert('Email and Password cannot be left blank.')
-        return
-      }
-      firebase.auth().signInWithEmailAndPassword(email.trim(), password)
-      .then(user => {
-        this.setState({ currentUser: user.user })
-        this.props.navigation.navigate('ChatList', { currentUser: user.user })
-      })
-      .catch(err => {
-        alert(err.message)
-      })
-    } catch(err) {
-      console.error(err)
-    }
->>>>>>> created working login page
   }
 
   render() {
@@ -138,10 +99,7 @@ export default class App extends Component {
               rounded
               success
               onPress={() => {this.loginUser(this.state.email, this.state.password)}}
-<<<<<<< 67419df7123eb30db764c0c9e34ad62346547165
               value={ this.state.password }
-=======
->>>>>>> created working login page
             >
               <Text style={{ color: 'white' }}>Login</Text>
             </Button>
