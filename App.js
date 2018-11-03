fd15394da47b629bc0aeb0f63e612df966720189
Chase Nelson <chase.a.nelson@gmail.com>
2018-11-03 10:38:46 -0500

    added firebase config

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import * as firebase from 'firebase'
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
 
-export default class App extends React.Component {
+// Init Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDtJxu-PAdSB5ahIY66eI0ziESkllxcJnE",
+  authDomain: "doublespeak-ba592.firebaseapp.com",
+  databaseURL: "https://doublespeak-ba592.firebaseio.com",
+  projectId: "doublespeak-ba592",
+  storageBucket: "doublespeak-ba592.appspot.com",
+}
+
+firebase.initializeApp(firebaseConfig);
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -16,6 +32,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
