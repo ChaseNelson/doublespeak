0839aa117291a30f16afb243f32dd4aeb3b0f463
Krista Strzeszkowski <krista.cathleen.722@gmail.com>
2018-11-04 06:22:26 -0600

    added chat to navigation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from 'react-navigation';
 import * as firebase from 'firebase';
 import Login from './Login';
 import ChatList from './ChatList';
+import Chat from './Chat';
 
 // Init Firebase
 const firebaseConfig = {
@@ -29,12 +30,18 @@ const navigator = createStackNavigator(
       },
     },
   },
+  Chat: {
+    screen: Chat,
+    navigationOptions: {
+      headerVisible: false
+    },
+  },
   {
     headerMode: 'none',
     navigationOptions: {
       headerVisible: false,
     },
-  }
-);
+  },
+});
 
 export default navigator;
