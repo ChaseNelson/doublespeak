50b672076c3273539290c967d7559db8c3eee9d8
Krista C Strzeszkowski <krista.cathleen.722@gmail.com>
2018-11-04 01:52:27 -0500

    Style login (#4)
    * Fun lang (#1)
    
    * init commit
    
    * removed
    
    * added emoji package
    
    * created emoji translated alphabet
    
    * init commit
    
    * removed
    
    * added emoji package
    
    * created emoji translated alphabet
    
    * tabs
    
    * revised and added new funny languages
    
    * added google translate
    
    * deleted
    
    * added functions to translate silly languages
    
    * had chaser language
    
    * Chat (#3)
    
    * added firebase
    
    * added firebase config
    
    * updated linter
    
    * added native-base
    
    * added eslint-plugin-prettier
    
    * created working login page
    
    * added lock files
    
    * added firebase
    
    * added firebase config
    
    * updated linter
    
    * added native-base
    
    * added eslint-plugin-prettier
    
    * created working login page
    
    * added lock files
    
    * Update Login.js
    
    * removed try catch and added own error checking
    
    * styalized login page

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Login from './Login'
+import ChatList from './ChatList'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import { createStackNavigator } from 'react-navigation'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator({
+  Login: {
+    screen: Login,
+    navigationOptions: {
+      title: 'Login',
+      headerLeft: null,
+    }
   },
-});
+  ChatList: {
+    screen: ChatList,
+    navigationOptions: {
+      title: 'Chat List',
+      headerLeft: null,
+    }
+  }
+})
+
+export default navigator
