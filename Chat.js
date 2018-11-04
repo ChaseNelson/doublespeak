1bfd397e9bc5aa60c1cb20c02b7fe1b0c20714e8
Krista Strzeszkowski <krista.cathleen.722@gmail.com>
2018-11-04 08:06:01 -0600

    created chat app

--- Chat.js
+++ Chat.js
@@ -2,32 +2,66 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import { GiftedChat } from 'react-native-gifted-chat';
 
-export default class Chat extends Component {
-  state = {
-    messages: [],
-  };
+import { StyleSheet, Text } from 'react-native';
+import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
 
-  componentWillMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-      ],
-    });
+export default class Chat extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      roomName: this.props.navigation.state.params.roomName,
+      currentUser: this.props.navigation.state.params.currentUser,
+      uid: this.props.navigation.state.params.uid,
+      messages: [],
+    };
   }
+
+  componentDidMount = () => {
+    firebase
+      .database()
+      .ref(`Rooms/${this.state.roomName}`)
+      .once('value')
+      .then(snapshot => {
+        const mess = snapshot.val().messages;
+        const messages = [];
+        for (const key in mess) {
+          alert(`mess[key] : ${JSON.stringify(mess[key])}`);
+          const temp = {
+            _id: 1,
+            text: mess[key].text,
+            createdAt: mess[key].timestamp,
+            user: {
+              _id: mess[key].sentBy.uid === this.state.uid ? 1 : 2,
+              name: mess[key].sentBy.name,
+            },
+          };
+          messages.push(temp);
+        }
+        this.setState({ messages });
+      })
+      .catch(err => {
+        alert(err.message);
+      });
+  };
 
   onSend(messages = []) {
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
     }));
+    firebase
+      .database()
+      .ref(`Rooms/${this.state.roomName}/messages/${new Date().getTime()}`)
+      .set({
+        timestamp: new Date().getTime(),
+        text: messages[messages.length - 1].text,
+        sentBy: {
+          uid: this.state.uid,
+          name: this.state.currentUser.displayName
+            ? this.state.currentUser.displayName
+            : this.state.currentUser.email,
+        },
+      });
   }
 
   render() {
@@ -37,6 +71,9 @@ export default class Chat extends Component {
         onSend={messages => this.onSend(messages)}
         user={{
           _id: 1,
+          name: this.state.currentUser.displayName
+            ? this.state.currentUser.displayName
+            : this.state.currentUser.email,
         }}
       />
     );
